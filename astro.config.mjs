9b72e2ad65775164a6efac0b98d11fbfdae4d5fb
Nathan <bitopsdev@gmail.com>
2026-04-29 00:50:30 -0400

    Ready to publish?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bitopsdev.com',
-
   output: 'static',
 
   adapter: cloudflare({
