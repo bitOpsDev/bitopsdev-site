e64841ed061ebc266d355061b629892b13eb4ebe
Nathan <bitopsdev@gmail.com>
2026-04-29 08:21:15 -0400

    Ready to publish?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,46 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bitopsdev.com',
-
-  // Pure static
-  output: 'static',
-
-  // No Cloudflare adapter for now
+  site: 'https://example.com',
   integrations: [mdx(), sitemap()],
 
-  adapter: cloudflare()
+  fonts: [
+      {
+          provider: fontProviders.local(),
+          name: 'Atkinson',
+          cssVariable: '--font-atkinson',
+          fallbacks: ['sans-serif'],
+          options: {
+              variants: [
+                  {
+                      src: ['./src/assets/fonts/atkinson-regular.woff'],
+                      weight: 400,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+                  {
+                      src: ['./src/assets/fonts/atkinson-bold.woff'],
+                      weight: 700,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+              ],
+          },
+      },
+	],
+
+  adapter: cloudflare({
+      platformProxy: {
+          enabled: true
+      },
+
+      imageService: "cloudflare"
+  }),
 });
